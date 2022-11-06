bcfca36b0f84d69758986fbec4b2c42d7af9a8a6
Oyinkansola Shoroye <oyinkansolashoroye@gmail.com>
2022-11-06 18:10:35 +0000

    init

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
+import {HelmetProvider} from 'react-helmet-async'
 import App from './App'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter><React.StrictMode>
+    <HelmetProvider>
 		<App />
+    </HelmetProvider>
 	</React.StrictMode></BrowserRouter>
 	
 )
--- src/reactRouter.jsx
+++ src/reactRouter.jsx
@@ -1,4 +1,4 @@
-import {useState, useRef} from 'react'
+import { useState, useRef } from 'react'
 import useFetch from './fetch'
 import {
   NavLink,
@@ -7,132 +7,180 @@ import {
   Outlet,
   useNavigate,
 } from "react-router-dom";
-import myImage from './oyinkansola.jfif'
-import {ErrorBoundary} from 'react-error-boundary'
+import {Helmet} from "react-helmet-async"
 
-function ErrorFallback({resetErrorBoundary}) {
+import myImage from './oyinkansola.jfif'
+import { ErrorBoundary } from 'react-error-boundary'
+
+
+function ErrorFallback({ resetErrorBoundary }) {
   return (
-    <div role="alert" style={{lineHeight:"40px"}}>
-      <p style={{fontSize:"40px", marginTop:"20px"}}>⚠️</p>
+    <div role="alert" style={{ lineHeight: "40px" }}>
+      <p style={{ fontSize: "40px", marginTop: "20px" }}>⚠️</p>
       <p>Looks like there was a problem</p>
       <div className="btn">
-     <p> 
-      <NavLink to="/">
-      <button className="nav_btn">Go to Admin Page</button>
-      </NavLink>  
-     </p>
-      <button className="nav_btn2" onClick={resetErrorBoundary} >Try Again</button> 
-    </div>
+        <p>
+          <NavLink to="/">
+            <button className="nav_btn">Go to Admin Page</button>
+          </NavLink>
+        </p>
+        <button className="nav_btn2" onClick={resetErrorBoundary} >Try Again</button>
+      </div>
     </div>
   )
 }
 
 function Navigation() {
   return (
+    <div className="fixed-nav">
     <section className="navigation">
-<NavLink style={({ isActive }) => isActive ? { color: "white" } : { color: "black" }} className="navy" to="/"> Admin </NavLink>
-      
-<NavLink style={({ isActive }) => isActive ? { color: "white" } : { color: "black" }} className="navy" to="/users"> Users </NavLink>
-      
-<NavLink style={({ isActive }) => isActive ? { color: "white" } : { color: "black" }} className="navy" to="/testerror"> Test Error </NavLink>
-      
-<NavLink style={({ isActive }) => isActive ? { color: "white" } : { color: "black" }} className="navy" to="/error"> 404 Page </NavLink>
+      <NavLink style={({ isActive }) => isActive ? { color: "white" } : { color: "black" }} className="navy" to="/"> Admin </NavLink>
+
+      <NavLink style={({ isActive }) => isActive ? { color: "white" } : { color: "black" }} className="navy" to="/data">Data </NavLink>
+
+      <NavLink style={({ isActive }) => isActive ? { color: "white" } : { color: "black" }} className="navy" to="/testerror"> Test Error </NavLink>
+
+      <NavLink style={({ isActive }) => isActive ? { color: "white" } : { color: "black" }} className="navy" to="/error"> 404 Page </NavLink>
     </section>
+      </div>
   );
 }
 
-export const Testerror=()=>{
+export const Testerror = () => {
   const [username, setUsername] = useState('')
   const usernameRef = useRef(null)
 
-  function Testerror({username}) {
-  if (username === 'incorrect') {
-    throw new Error('')
+  function Testerror({ username }) {
+    if (username === 'incorrect') {
+      throw new Error('')
+    }
+    return ``
   }
-  return ``
-}
-  
-  return( <div>
+
+  return (
+    <div>
+      <Helmet>
+        <title>Test Error</title>
+        <meta
+      name="test error"
+      content="this page is used for testing error"
+    />
+        <link rel="canonical" href="/testerror" />
+      </Helmet>
     <label>
-      <p style={{marginTop:"20px", fontSize:"20px"}}>Wrong password is <b>'incorrect'</b></p>
+      <p style={{ marginTop: "20px", fontSize: "20px" }}>Wrong password is <b>'incorrect'</b></p>
       <input placeholder={`enter the wrong password provided above`} value={username} onChange={e => setUsername(e.target.value)} ref={usernameRef} />
-      </label>
+    </label>
     <ErrorBoundary
-          FallbackComponent={ErrorFallback}
-          onReset={() => {
-            setUsername('')
-            usernameRef.current.focus()
-          }}
-          resetKeys={[username]}
-        >
+      FallbackComponent={ErrorFallback}
+      onReset={() => {
+        setUsername('')
+        usernameRef.current.focus()
+      }}
+      resetKeys={[username]}
+    >
       <Testerror username={username} />
-      </ErrorBoundary>
+    </ErrorBoundary>
     <Navigation />
   </div>)
 }
 
+
 export function Home() {
+
   return (
+    <>
+      <Helmet>
+        <title>Admin</title>
+        <meta
+      name="admin"
+      content="this admin page has a google login auth"
+    />
+        <link rel="canonical" href="/" />
+      </Helmet>
     <div>
-      <h1 style={{marginTop:"25px"}}>Dashboard</h1>
-      <p style={{color:"#3498db", marginTop:"5px"}}>Admin Dashboard</p>
-      <img style={{margin:"35px"}} src={myImage} width="180px" height="180px"/>
-      <p style={{margin:"8px"}}> <b>Oyinkansola Shoroye </b><br/> Frontend Engineer</p>
+      <h1 style={{ marginTop: "25px" }}>Dashboard</h1>
+      <p style={{ color: "#3498db", marginTop: "5px" }}>Admin Dashboard</p>
+      <img style={{ margin: "35px" }} src={myImage} width="180px" height="180px" />
+      <p style={{ margin: "8px" }}> <b>Oyinkansola Shoroye </b><br /> Frontend Engineer</p>
       <Navigation />
     </div>
+
+    </>
   );
 }
 
-export function Users() {
- const [page, setPage] = useState(1);
+export function Data() {
+  const [page, setPage] = useState(1);
   const { loading, error, data } = useFetch(`https://randomuser.me/api/?page=${page}&results=10&seed=abc`);
 
   console.log({ loading, error, data });
 
   const pages = 20;
-  
+
   if (loading) {
     return <>Loading...</>;
   }
-
+ 
   return (
     <div className="App">
-      <h1 className="title" style={{color:"#3498db", marginTop:"15px"}}>New Users</h1>
-      
+      <Helmet>
+        <title>User's Data</title>
+        <meta
+      name="user's data"
+      content="A list of user's data"
+    />
+        <link rel="canonical" href="/data" />
+      </Helmet>
+      <h1 className="title" style={{ color: "#3498db", marginTop: "15px", marginBottom: "20px" }}>User's Data</h1>
+<div className='info' style={{ background: "#97CAED", color: "white" }}>
+  
+  <p>First name</p> 
+  <p>Last name</p> 
+  <p>Age</p> 
+  <p>Gender</p> 
+  <p>Username</p> 
+  <p>Password</p>
+</div>
       {data?.results
-        
+
         .map((each, index) => {
-          const name = `${each.name.title} ${each.name.first} ${each.name.last}`;
+          const name = `${each.name.first} ${each.name.last}`;
           return (
+            
             <div className='info' key={name.toLowerCase().replaceAll(' ', '')}>
-             <p><img src={each.picture.medium}/></p>Name: {`${name}`} 
-               <p>Date of Birth: {each.dob.date.substr(0,10)} </p>
-              <p> Country: {each.location.country}</p>
+
+               <p>{each.name.first}</p>
+               <p>{each.name.last}</p>
+              <p>{each.dob.age}</p>
+              <p>{each.gender}</p>
+              <p>{each.login.username}</p>
+              <p>{each.login.password}</p>
             </div>
           );
         })}
-     
-      <p className="pagination">
+
+      <p className="pagination" style={{ marginTop: "30px" }}>
         Pages: {page} of {pages}
       </p>
-       {
+      {
         <button role="button"
           className="next_btn"
           aria-pressed="false"
           tabindex="0"
           disabled={page <= 1}
           onClick={() => setPage((prev) => prev - 1)}
-         style={{background:"#3498db",color:"black"}}
+          style={{ background: "#3498db", color: "black" }}
         >
           prev
         </button>
       }
-    
+
       {Array.from({ length: pages }, (value, index) => index + 1).map(
         (each) => (
           <button role="button"
-          aria-pressed="false"
-          tabindex="0" className="user_btn" onClick={() => setPage(each)}>{each}</button>
+            aria-pressed="false"
+            tabindex="0" className="user_btn" onClick={() => setPage(each)}>{each}</button>
         )
       )}
       {
@@ -140,14 +188,14 @@ export function Users() {
           disabled={page >= pages}
           aria-disabled={page >= pages}
           onClick={() => setPage((prev) => prev + 1)}
-          style={{backgroundColor:"#3498db",color:"black"}}
+          style={{ backgroundColor: "#3498db", color: "black" }}
         >
           next
         </button>
       }
-      
-      <div style={{margin:"15px"}}><Navigation /></div>
-       <Outlet/>
+
+      <div style={{ margin: "15px" }}><Navigation /></div>
+      <Outlet />
     </div>
   );
 }
@@ -173,22 +221,30 @@ function Another() {
 const Errorpage = () => {
   return (
     <div>
+      <Helmet>
+        <title>404 Error</title>
+        <meta
+      name="404 Error"
+      content="an error occurred"
+    />
+        <link rel="canonical" href="/error" />
+      </Helmet>
       <p style={{ color: "grey", fontSize: "100px", textAlign: "center" }}> Oops!
       </p>
-      <p style={{ color: "black", fontSize: "23px", textAlign: "center", marginBottom: "20px" }}> We can't seem to find the <br/> page you're looking for.
+      <p style={{ color: "black", fontSize: "23px", textAlign: "center", marginBottom: "20px" }}> We can't seem to find the <br /> page you're looking for.
       </p>
       <p style={{ color: "black", fontSize: "16px" }}>
         Error code: 404
       </p>
       <p className='errors'>Here are some helpful links instead:</p>
-      
-<NavLink style={({ isActive }) => isActive ? { color: "white" } : { color: "black" }} className="navy" to="/"> Admin </NavLink>
-      
-<NavLink style={({ isActive }) => isActive ? { color: "white" } : { color: "black" }} className="navy" to="/users"> Users </NavLink>
-      
-<NavLink style={({ isActive }) => isActive ? { color: "white" } : { color: "black" }} className="navy" to="/testerror"> Test Error </NavLink>
-      
-<NavLink style={({ isActive }) => isActive ? { color: "white" } : { color: "black" }} className="navy" to="/error"> 404 Page </NavLink>
+
+      <NavLink style={({ isActive }) => isActive ? { color: "white" } : { color: "black" }} className="navy" to="/"> Admin </NavLink>
+
+      <NavLink style={({ isActive }) => isActive ? { color: "white" } : { color: "black" }} className="navy" to="/data"> Data </NavLink>
+
+      <NavLink style={({ isActive }) => isActive ? { color: "white" } : { color: "black" }} className="navy" to="/testerror"> Test Error </NavLink>
+
+      <NavLink style={({ isActive }) => isActive ? { color: "white" } : { color: "black" }} className="navy" to="/error"> 404 Page </NavLink>
     </div>
   );
 };
@@ -198,11 +254,11 @@ function Path() {
   return (
     <div className='nav'>
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/testerror" element={<Testerror/>}/>
-        <Route path="/users" element={<Users />}>
-        <Route path="out" element={<Myout />} />
-        <Route path=":another" element={<Another />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/testerror" element={<Testerror />} />
+        <Route path="/data" element={<Data />}>
+          <Route path="out" element={<Myout />} />
+          <Route path=":another" element={<Another />} />
         </Route>
         <Route path="*" element={<Errorpage />} />
       </Routes>
@@ -210,3 +266,7 @@ function Path() {
   );
 }
 export default Path;
+
+
+
+
